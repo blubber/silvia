93dd049182d7d36904e30382147afa607995204d
Tiemo Kieft <t.kieft@infinitesimal.nl>
2014-11-12 18:05:22 +0100

    model.system: ran through flake8

--- system.py
+++ system.py
@@ -11,7 +11,7 @@ import math
 # System Parameters
 
 #: Heat capacity of the water inside the boiler.
-Cw = 0.23 * 4176.8 # 330 mL of water
+Cw = 0.23 * 4176.8
 
 #: Radius of the boiler (in m).
 r = (7.7/2.0) * 0.01
@@ -29,7 +29,8 @@ rhoBrass = 8520
 cpBrass = 377
 
 #: Heat capacity of the boiler.
-Cm =  ((math.pi * r**2  - math.pi * (r - d/2)**2) * h + 2 * (math.pi * r**2 * d)) * rhoBrass * cpBrass
+Cm = ((math.pi * r**2 - math.pi * (r - d/2)**2) *
+      h + 2 * (math.pi * r**2 * d)) * rhoBrass * cpBrass
 
 #: Resistance between brass and boiler.
 Rf = 0.15
@@ -47,12 +48,14 @@ heater_power = 980
 # -------------------------------------------------------------------------
 # Model
 
-def model (fn, y, t):
+def model(fn, y, t):
     ''' Simulates the temperature of the water inside the boiler at `t`.
 
     :param fn: Should be a callable that takes the current time in seconds
                and the current water temperatur in degrees Celsius, and
                returns the desired heater power.
     '''
-    return [((-1/(Rf*Cw))*y[0] + (1/(Cw* Rf))*y[1] + fn(t, y[1])/Cw), \
-        ((1/(Rf*Cm))*y[0] -(1/(Rf*Cm) + 1/(Re*Cm))*y[1] +(1/(Re*Cm)) * Te )]
+    return [
+        ((-1 / (Rf*Cw)) * y[0] + (1 / (Cw * Rf)) * y[1] + fn(t, y[1]) / Cw),
+        ((1 / (Rf * Cm)) * y[0] - (1 / (Rf * Cm) + 1 /
+         (Re * Cm)) * y[1] + (1 / (Re * Cm)) * Te)]
