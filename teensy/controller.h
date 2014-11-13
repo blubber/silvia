95306508b89002af259ea5599f04d2938a9bb900
Tiemo Kieft <t.kieft@gmail.com>
2014-11-13 13:35:13 +0100

    teensy/controller.h: add function defs to header

--- teensy/controller.h
+++ teensy/controller.h
@@ -1,6 +1,7 @@
 #ifndef __CONTROLLER_H__
 #define __CONTROLLER_H__ 1
 
+typedef unsigned long uint32_t;
 typedef struct {
     float     setpoint;
     float     Kp;
@@ -10,5 +11,9 @@ typedef struct {
     float     integral;
     float     previous_error;
 } ControllerContext;
+
+ControllerContext controller_new (float setpoint, float Kp,
+                                  float Ki, float Kd);
+float controller_power (ControllerContext *ctx, long dt, float T);
 
 #endif /* __CONTROLLER_H__ */
