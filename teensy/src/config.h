799101cb9665f7b6dfaa3499e474187347169e79
Tiemo Kieft <t.kieft@gmail.com>
2014-11-29 09:47:42 +0100

    teensy/config.h: move configuration to separate .h file

---
+++ teensy/src/config.h
@@ -0,0 +1,20 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_ 1
+
+
+#define SETPOINT         70
+#define CYCLE            500
+
+#define KP               0.045
+#define KI               0.00001
+#define KD               0
+
+#define THERMO_DO        21
+#define THERMO_CS        20
+#define THERMO_CLK       19
+
+#define HEATER_PIN       3
+#define PUMP_PIN         4
+
+
+#endif
